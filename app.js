6f59054f8e969e197ad211f6d94a1f2cc383c5fe
vvictorious <vvictorious30@gmail.com>
2018-05-28 10:31:30 -0600

    bootstrap is up and running

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 
 const app = express();
+
+//handlebars middleware
+app.engine('handlebars', exphbs({
+	defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+
+//index route
+app.get('/', (req, res) =>{
+	const title = 'Welcome';
+	res.render('index', {
+	title: title
+	});
+});
+
+//ABOUT ROUTE
+app.get('/about', (req,res) =>{
+	res.render('about');
+})
 
 const port = 5000;
 
