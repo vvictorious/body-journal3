9b384300a3f7524e904325b8d9c8591bf9559a0b
vvictorious <vvictorious30@gmail.com>
2018-05-28 18:35:03 -0600

    page is rerendering if form is submitted without any inputs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -19,6 +20,9 @@ app.engine('handlebars', exphbs({
 }));
 app.set('view engine', 'handlebars');
 
+//bodyParser middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 //index route
 app.get('/', (req, res) =>{
@@ -38,6 +42,27 @@ app.get('/ideas/add', (req,res) =>{
 	res.render('ideas/add');
 })
 
+
+//process form
+app.post('/ideas', (req, res) =>{
+	let errors = [];
+	if(!req.body.title){
+		errors.push({text:'Please add what you ate today.'});
+	}
+	if(!req.body.details){
+	errors.push({text:'Please add your workout.'});
+	}
+
+	if(errors.length > 0){
+		res.render('ideas/add', {
+			errors: errors,
+			title: req.body.title,
+			details: req.body.details
+		});
+	} else {
+		res.send('passed');
+	}
+})
 
 
 const port = 5000;
