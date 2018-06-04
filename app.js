a823e1e46ca28a9d0c2836f019b1889ef6f54dbd
vvictorious <vvictorious30@gmail.com>
2018-06-04 16:28:59 -0600

    things are still working somehow

--- app.js
+++ app.js
@@ -49,6 +49,10 @@ app.use(session({
   saveUnitialized: true,
 }));
 
+//passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 //connect-flash middleware
 app.use(flash());
 
@@ -57,6 +61,7 @@ app.use(function(req, res, next){
     res.locals.success_msg = req.flash('success_msg');
     res.locals.error_msg = req.flash('error_msg');
     res.locals.error = req.flash('error');
+    res.locals.user = req.user || null;
     next();
 })
 
