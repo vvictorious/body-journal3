161775dd3ba49e448b53c49c3abcf3d8dd56b0b7
vvictorious <vvictorious30@gmail.com>
2018-06-04 16:46:02 -0600

    logout works

--- routes/users.js
+++ routes/users.js
@@ -76,4 +76,11 @@ router.post('/register', (req, res) =>{
 	}
 });
 
+//logout route
+router.get('/logout', (req, res) =>{
+	req.logout();
+	req.flash('success_msg', 'You are logged out');
+	res.redirect('/users/login');
+});
+
 module.exports = router;
