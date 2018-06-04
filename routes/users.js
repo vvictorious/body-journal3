fb71c39088ab9fed52fc04b2ffa386ce4ef88f86
vvictorious <vvictorious30@gmail.com>
2018-06-04 11:01:40 -0600

    validation is up and running

--- routes/users.js
+++ routes/users.js
@@ -9,7 +9,29 @@ router.get('/login', (req, res) =>{
 
 //user register route
 router.get('/register', (req, res) =>{
-  res.send('register');
+  res.render('users/register');
+});
+
+// register form POST
+router.post('/register', (req, res) =>{
+	let errors = [];
+	if(req.body.password != req.body.password2){
+		errors.push({text: 'Passwords do not match'});
+	}
+	if(req.body.password.length < 4){
+		errors.push({text: 'Password must be at least 4 charachters'});
+	}
+	if(errors.length > 0){
+		res.render('users/register', {
+			errors: errors,
+			name: req.body.name,
+			email: req.body.email,
+			password: req.body.password,
+			password2: req.body.password2
+		});
+	} else {
+		res.send('passed');
+	}
 });
 
 module.exports = router;
